776951c7e22e72c7510e55faf6cf5545981f613b
Randall Christ <cr999c999@gmail.com>
2023-04-07 20:13:49 -0700

    Added HTTP config to fetchData

--- lib/helpers/network.ts
+++ lib/helpers/network.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 /**
  * fetches API data from The Lord of the Rings server
  * @param url {String} API URL
  * @returns Promise<Response>
  */
-const fetchData = (url) => {
-  return axios.get(url);
+const fetchData = (url: string, config: AxiosRequestConfig<any>) => {
+  return axios.get(url, config);
 };
 
 export default {
